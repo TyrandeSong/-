7c0369b3db21d67c9c62b47d9264c657c0705d0b
TyrandeSong <TyrandeSong@boyaa.com>
2018-05-30 10:50:10 +0800

    测试

---
+++ test.php
@@ -0,0 +1,16 @@
+ <?php
+ error_reporting(0);
+ function countcows($years) {
+        $cows[] = 0;
+        if($years < 4) return 1;
+        for($i=4; $i <= $years; $i++) {
+            for($j=0, $k=count($cows); $j<$k; $j++) {
+                $age = $i - $cows[$j];
+                if($age >= 4 && $age < 15) $cows[] = $i;
+                else if($age == 20) unset($cows[$j]);
+            }
+        }
+        return count($cows);
+    }
+
+var_dump(countcows(13));
